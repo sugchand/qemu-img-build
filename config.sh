c815cbeba70293635a413142d30e7d90394f5d4a
Sugesh Chandran <sugesh.chandran@intel.com>
2017-08-13 20:32:25 +0100

    Adding functions to support image build for qemu.
    Updated the Linux image with proper settings. This commit can create
    a valid bootable qemu qcow image.
    
    Signed-off-by: Sugesh Chandran <sugesh.chandran@intel.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,13 @@
 # Configuration file for qcow-img-burner.
 
 #Default set of application, Advised to keep the list as is.
-DEFAULT_IMG_APPS=build-essential,msr-tools,libssl-dev,openssh-server,openssh-client,openssl,unzip,wget,python
+#DEFAULT_IMG_APPS=build-essential,msr-tools,libssl-dev,openssh-server,openssh-client,openssl,unzip,wget,python
 
 #Tools to install in the VM image, append to the list for more apps seperated by comma.
 IMG_APPS=vim,git
 
+#Linux image headers and tools, use specific version in need.
+IMG_KERNEL_APPS=linux-image-generic,linux-headers-generic,linux-tools-generic
 #Image mirror to pull the debian system from.
 IMG_MIRROR_URL=http://archive.ubuntu.com/ubuntu/
 
@@ -24,3 +26,5 @@ HTTP_PROXY=''
 HTTPS_PROXY=''
 FTP_PROXY=''
 
+#VM image root password, must set to login
+ROOT_PWD='tester'
--- debian-img-burner.sh
+++ debian-img-burner.sh
@@ -10,6 +10,7 @@ DISK_MNT=/mnt/qemu-disk
 export http_proxy=$HTTP_PROXY
 export https_proxy=$HTTPS_PROXY
 export ftp_proxy=$FTP_PROXY
+CHROOT_SCRIPT=chroot-run.sh
 ##############################################################################
 
 function install_prereq {
@@ -18,10 +19,13 @@ function install_prereq {
 
 # Clean up function to clear out system state.
 function cleanup() {
+    umount $DISK_MNT/proc
+    umount $DISK_MNT/sys
+    umount $DISK_MNT/dev
+    umount $DISK_MNT
     umount /dev/nbd0p2
-    rm -rf $DISK_MNT
     qemu-nbd -d /dev/nbd0
-    rm -rf $QEMU_IMG_NAME
+    #rm -rf $QEMU_IMG_NAME
 }
 
 # Pass the error code, exit-state and error string to the function.
@@ -38,6 +42,7 @@ function exit_on_error() {
     fi
     if [ $ret_code -ne 0 ] && [ $is_exit -ne 0 ]; then
         echo "** EXITING THE PROGRAM ERR: $ret_code **"
+        cleanup
         exit $ret_code
     fi
 }
@@ -51,6 +56,73 @@ p
 w
 
 EEOF
+}
+
+# Setting the proxy inside the VM apt.
+function set_apt_proxy() {
+    if [[ -v HTTP_PROXY ]]; then
+        proxy_setup="Acquire::http::Proxy \"$HTTP_PROXY\";"
+        echo $proxy_setup >  $DISK_MNT/etc/apt/apt.conf
+    fi
+}
+
+# Create a script file to run commands inside the chroot.
+function chroot_script() {
+set_apt_proxy
+cat >$DISK_MNT/$CHROOT_SCRIPT <<EOL
+
+#!/bin/bash -x
+
+mount -t proc none /proc
+
+mount -t sysfs none /sys
+
+export PATH=$PATH:/usr/bin:/usr/sbin/
+
+export http_proxy=${HTTP_PROXY}
+export https_proxy=${HTTPS_PROXY}
+export ftp_proxy=${FTP_PROXY}
+
+#Make apt install non interactive
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get install -y grub-pc
+
+grub-install --force /dev/nbd0 --modules="biosdisk part_msdos"
+
+
+apt-get install -y sed
+
+# Update the grub command line options.
+sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/d' /etc/default/grub
+echo 'GRUB_CMDLINE_LINUX_DEFAULT="nomodeset"' >> /etc/default/grub
+
+update-grub
+
+echo "Updating root password"
+
+echo "${ROOT_PWD}\n${ROOT_PWD}" | passwd root
+
+umount /proc/ /sys/ /dev/
+
+EOL
+
+}
+
+function chroot_setup() {
+    mount --bind /dev/ $DISK_MNT/dev
+    exit_on_error $? 1 "Failed to mount dev"
+    chroot_script
+    chmod a+rwx $DISK_MNT/$CHROOT_SCRIPT
+    LANG=C chroot $DISK_MNT ./$CHROOT_SCRIPT
+
+    umount $DISK_MNT/dev
+}
+
+# Function to update VM grub with disk entry.
+function update_vm_grubcfg() {
+    sed -i -e 's/nbd0p2/sda2/g' $DISK_MNT/boot/grub/grub.cfg
+    exit_on_error $? 1 "ERR: Failed to update grub,"
 }
 
 # function to attach the image to nbd and format it.
@@ -67,8 +139,8 @@ function nbd_attach_format_install {
     mkdir -p $DISK_MNT
     mount /dev/nbd0p2 $DISK_MNT
     exit_on_error $? 1 "ERR: Failed to mount disk,"
-    debootstrap --include=$DEFAULT_IMG_APPS,$IMG_APPS $IMG_SUITE $DISK_MNT \
-    $IMG_MIRROR_URL
+    debootstrap --include=$IMG_KERNEL_APPS,$DEFAULT_IMG_APPS,$IMG_APPS \
+    $IMG_SUITE $DISK_MNT $IMG_MIRROR_URL
 }
 
 function create_qcow_disk {
@@ -92,6 +164,7 @@ function create_qcow_disk {
         ;;
     esac
     echo "Creating the disk of size $QCOW_SIZE and diskfile $gpt"
+    rm -rf $QEMU_IMG_NAME
     qemu-img create -f qcow2 $QEMU_IMG_NAME $QCOW_SIZE
     exit_on_error $? 1 "ERR:Failed to create qemu-img"
 
@@ -102,6 +175,9 @@ function main {
     cleanup
     create_qcow_disk
     nbd_attach_format_install
+    chroot_setup
+    update_vm_grubcfg
+    cleanup
 }
 
 main
